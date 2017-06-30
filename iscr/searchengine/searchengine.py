97cff995c4c3181abc215d1fbeff4f75c2ec7799
Antonie Lin <iammrhelo@gmail.com>
2017-07-01 00:51:37 +0800

    Assign result to search engine self

--- iscr/searchengine/searchengine.py
+++ iscr/searchengine/searchengine.py
@@ -16,65 +16,67 @@ class SearchEngine(object):
 		print("[SearchEngine] Loading index from {}".format(index_pickle))
 		self.indices = load_from_pickle(index_pickle)
 
-	def retrieve(self, query, negquery=None, alpha=1000, beta=0.1):
+		self.docnames = self.indices['doclengs'].keys()
+
+	def refresh_results(self):
+		self.result = {}
+		for docname in self.indices['doclengs'].keys():
+			self.result[docname] = sys.float_info.min
+
+	def retrieve(self, query, negquery=None):
 		"""
 			Returns a list of tuples [(docname, score),...]
 			For MAP, every docname has to exist
 		"""
-		# This is the return object
-		result = {}
-		docnames = self.indices['doclengs'].keys()
-		for docname in docnames:
-			result[docname] = sys.float_info.min
+		self.refresh_results()
 
-		def _retrieve(query, entropy_weight=1.):
-			# Query
-			for wordID, word_prob in query.items():
-				# Check if query word intersects with documents
-				if wordID not in self.indices['background']:
-					continue
-
-				# Recored scored documents for this word
-				unscored_docs = set(docnames)
-
-				word_background_prob = self.indices['background'][wordID]
-				word_inverted_index = self.indices['inverted_index'][wordID].items()
-				for docID, docprob in word_inverted_index:
-					unscored_docs.remove(docID)
-
-					doclength = self.indices['doclengs'][ docID ]
-
-					weighted_entropy = entropy_weight * \
-						entropy_between_word_and_smoothed_doc(
-							word_prob, docprob, doclength, word_background_prob)
-
-					# Adds to result
-					if result[docID] != sys.float_info.min:
-						result[docID] += weighted_entropy
-					else:
-						result[docID] = weighted_entropy
-
-				# Smooth background for those unscored
-				for docID in unscored_docs:
-					doclength = self.indices['doclengs'][ docID ]
-
-					weighted_entropy = entropy_weight * \
-						entropy_between_word_and_smoothed_doc(
-							word_prob, 0., doclength, word_background_prob)
-
-					# Adds to result
-					if result[docID] != sys.float_info.min:
-						result[docID] += weighted_entropy
-					else:
-						result[docID] = weighted_entropy
-
-		_retrieve(query, 1)
+		self._retrieve(query, 1)
 		if negquery is not None:
-			_retrieve(negquery, -0.1)
+			self._retrieve(negquery, -0.1)
 
-		sorted_ret = sorted(result.items(), key=lambda x: x[1], reverse=True)
+		sorted_ret = sorted(self.result.items(), key=lambda x: x[1], reverse=True)
 		return sorted_ret
 
+	def _retrieve(self, query, entropy_weight=1.):
+		# Query
+		for wordID, word_prob in query.items():
+			# Check if query word intersects with documents
+			if wordID not in self.indices['background']:
+				continue
+
+			# Recored scored documents for this word
+			unscored_docs = set(self.docnames)
+
+			word_background_prob = self.indices['background'][wordID]
+			word_inverted_index = self.indices['inverted_index'][wordID].items()
+			for docID, docprob in word_inverted_index:
+				unscored_docs.remove(docID)
+
+				doclength = self.indices['doclengs'][ docID ]
+
+				weighted_entropy = entropy_weight * \
+					entropy_between_word_and_smoothed_doc(
+						word_prob, docprob, doclength, word_background_prob)
+
+				# Adds to result
+				if self.result[docID] != sys.float_info.min:
+					self.result[docID] += weighted_entropy
+				else:
+					self.result[docID] = weighted_entropy
+
+			# Smooth background for those unscored
+			for docID in unscored_docs:
+				doclength = self.indices['doclengs'][ docID ]
+
+				weighted_entropy = entropy_weight * \
+					entropy_between_word_and_smoothed_doc(
+						word_prob, 0., doclength, word_background_prob)
+
+				# Adds to result
+				if self.result[docID] != sys.float_info.min:
+					self.result[docID] += weighted_entropy
+				else:
+					self.result[docID] = weighted_entropy
 
 def smooth_weight_from_docleng(length, alpha=1000):
 	return length / (length + alpha)
