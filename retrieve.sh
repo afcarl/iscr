4e5bdaa2f6bc3cd12780a250b03ffc9a6bd4575c
Antonie Lin <iammrhelo@gmail.com>
2017-06-30 14:45:14 +0800

    Rename to 'collections'

--- retrieve.sh
+++ retrieve.sh
@@ -1,8 +1,8 @@
 echo "Trying to reproduce onebest CMVN paper results..."
-python scripts/test_retrieval.py -q ./queries/PTV.onebest.CMVN.paper.query.pickle -d ./iscr/searchengine/data/PTV.onebest.CMVN.paper/
+python scripts/test_retrieval.py -q ./queries/PTV.onebest.CMVN.paper.query.pickle -d ./iscr/searchengine/collections/PTV.onebest.CMVN.paper/
 
-echo "Trying to reproduce lattice CMVN paper results..."
-python scripts/test_retrieval.py -q ./queries/PTV.lattice.CMVN.paper.query.pickle -d ./iscr/searchengine/data/PTV.lattice.CMVN.paper/
+#echo "Trying to reproduce lattice CMVN paper results..."
+#python scripts/test_retrieval.py -q ./queries/PTV.lattice.CMVN.paper.query.pickle -d ./iscr/searchengine/collections/PTV.lattice.CMVN.paper/
 
 echo "DNN onebest jieba results"
-python scripts/test_retrieval.py -q ./queries/PTV.dnn.onebest.jieba.query.pickle -d ./iscr/searchengine/data/PTV.dnn.onebest.jieba
+python scripts/test_retrieval.py -q ./queries/PTV.dnn.onebest.jieba.query.pickle -d ./iscr/searchengine/collections/PTV.dnn.onebest.jieba
