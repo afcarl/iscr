98422ae5b0355c28b3e21a120278d694a82e5496
Antonie Lin <iammrhelo@gmail.com>
2017-07-01 13:38:06 +0800

    unused multithread retrieve

--- iscr/searchengine/searchengine.py
+++ iscr/searchengine/searchengine.py
@@ -1,8 +1,11 @@
 from collections import defaultdict
 import os
+from operator import itemgetter
 import sys
 import time
-
+"""
+from pathos.pools import ThreadPool
+"""
 from .metrics import normalize, cross_entropy
 from ..utils import load_from_pickle
 
@@ -17,6 +20,8 @@ class SearchEngine(object):
 		self.indices = load_from_pickle(index_pickle)
 
 		self.docnames = self.indices['doclengs'].keys()
+
+		self.pool = ThreadPool(10)
 
 		self.result = None
 
@@ -65,7 +70,48 @@ class SearchEngine(object):
 					cross_entropy(word_prob, smoothed_docprob)
 				# Add to result
 				self.result[docID] += weighted_entropy
+	"""
+	def _calculate_document_scores_pool(self, query, entropy_weight=1.):
 
+		# Query
+		def docprob_generator(word_prob, word_background_prob, word_inverted_index):
+			for docname in self.docnames:
+				# Get doc prob if in inverted_index, else set to 0.
+				docprob = word_inverted_index.get(docname, 0.)
+
+				doclength = self.indices['doclengs'][ docname ]
+
+				yield docname, word_prob, docprob, doclength, word_background_prob
+
+		def docscore_job(param):
+			docname, word_prob, docprob, doclength, word_background_prob = param
+			# Get doc prob if in inverted_index, else set to 0.
+			docprob = word_inverted_index.get(docname, 0.)
+
+			doclength = self.indices['doclengs'][ docname ]
+
+			smoothed_docprob = smooth_docprob(
+				docprob, doclength, word_background_prob)
+
+			weighted_entropy = entropy_weight * \
+				cross_entropy(word_prob, smoothed_docprob)
+			return docname, weighted_entropy
+
+
+		for wordID, word_prob in query.items():
+			# Check if query word intersects with documents
+			if wordID not in self.indices['background']:
+				continue
+
+			# Recored scored documents for this word
+			word_background_prob = self.indices['background'][wordID]
+			word_inverted_index = self.indices['inverted_index'][wordID]
+
+			scores = self.pool.map(docscore_job, docprob_generator(word_prob, word_background_prob, word_inverted_index))
+
+			for docname, weighted_entropy in scores:
+				self.result[ docname ] += weighted_entropy
+	"""
 
 def smooth_docprob(docprob, doclength, word_background_prob, alpha=1000):
 	# Get smoothing weight
